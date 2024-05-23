fd8fe11f315d227d2452c7e061e644f187bf8e5f
danyjim <dany_jimenez_v@hotmail.com>
2024-05-23 09:00:41 -0600

    injector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { SqlinjectorService, Documento } from './sqlinjector.service';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'abeja';
+  documentos!: Documento[];
+  constructor(private SqlinjectorService: SqlinjectorService, private router: Router, private activateRoute: ActivatedRoute){}
+
+
+  this.SqlinjectorService.obtenerDocumentos().subscribe()(
+    res=>{
+      console.log(res)
+      this.documentos=<any>res;
+    },
+    err=>console.log(err)
+  )
+  
 }
